305029271a8b2c7113ff17005438684771b75e90
Viktor Horyachev <govis@msn.com>
2026-08-21 18:33:19 -0400

    fix(backend): Correct Locations node accessor for GetPath

--- backend/Data/Cache/Featured/Company.cs
+++ backend/Data/Cache/Featured/Company.cs
@@ -32,7 +32,7 @@ namespace BizSrt.Api.Data.Cache.Featured
 
             if (key.Item2 > 0)
             {
-                var locIds = BizSrt.Api.Data.Cache.LegacyCache.Locations.GetPath(key.Item2).Select(i => i.Id).ToArray();
+                var locIds = BizSrt.Api.Data.Cache.LegacyCache.Locations[key.Item2].GetPath(null).Select(i => i.Id).ToArray();
 
                 cq = from c in cq
                      where dbContext.CompanyOffices
